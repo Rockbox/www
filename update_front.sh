80ff93414fda8403e09ec8fd3ae21322db23820b
Björn Stenberg <bjorn@haxx.se>
2008-03-08 23:07:01 +0000

    svn2html moved to tools/
    git-svn-id: svn://svn.rockbox.org/rockbox/www@16567 a1c6a512-1295-4272-9138-f99709370657

--- update_front.sh
+++ update_front.sh
@@ -4,10 +4,10 @@
 perl last20threads.pl 5 1 > threads_front.html
 
 # recent commits
-( cd ~dast/src/rockbox && svn log -v --limit 5 ) | perl svnlog2html.pl > last5front.html
+( cd ~dast/src/rockbox && svn log -v --limit 5 ) | perl tools/svnlog2html.pl > last5front.html
 
 # more commits
-( cd ~dast/src/rockbox && svn log -v --limit 15 ) | perl svnlog2html.pl > lastsvn.html
+( cd ~dast/src/rockbox && svn log -v --limit 15 ) | perl tools/svnlog2html.pl > lastsvn.html
 
 # recent wiki edits
 perl recentwiki.pl 5 > recentwiki_front.html
