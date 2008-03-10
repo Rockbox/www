77204da722680a3e8000ee0dad92e8125af507a0
Björn Stenberg <bjorn@haxx.se>
2008-03-10 13:31:18 +0000

    Added creation of all 'recent' files.
    git-svn-id: svn://svn.rockbox.org/rockbox/www@16608 a1c6a512-1295-4272-9138-f99709370657

--- update_front.sh
+++ update_front.sh
@@ -2,12 +2,12 @@
 
 # recent mails
 perl last20threads.pl 5 1 > threads_front.html
+perl last20threads.pl 20 1 > threads.html
 
 # recent commits
 ( cd ~dast/src/rockbox && svn log -v --limit 5 ) | perl tools/svnlog2html.pl > last5front.html
-
-# more commits
 ( cd ~dast/src/rockbox && svn log -v --limit 15 ) | perl tools/svnlog2html.pl > lastsvn.html
 
 # recent wiki edits
 perl recentwiki.pl 5 > recentwiki_front.html
+perl recentwiki.pl 20 > recentwiki.html
