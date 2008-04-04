22459faa6eb8ba65e5fca9115d44d294f007f81c
Björn Stenberg <bjorn@haxx.se>
2008-04-04 21:29:19 +0000

    Text link handling script
    git-svn-id: svn://svn.rockbox.org/rockbox/www@16968 a1c6a512-1295-4272-9138-f99709370657

---
+++ tools/textlinks.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+# get text links from xml feed and produce html
+
+my $url = "http://update.livecustomer.net/?key=a539610bf93ee0795156fa7055d4b222&version=2.0.0&include_id=4696";
+
+my @output = `curl --silent --max-time 5 "$url"`;
+my $output = join '', @output;
+
+my @lines = split '<Link', $output;
+my @list;
+
+for my $line (@lines) {
+    if ($line =~ /url><!\[cdata\[(.+?)\]/i) {
+        my $url = $1;
+ 
+        if ($line =~ /description><!\[cdata\[(.+?)\]/i) {
+            push @list, "<a href='$url'>$1</a>";
+        }
+    }
+}
+
+my $html = join ",\n", @list;
+
+print <<END
+<p>&nbsp;<p>&nbsp;
+<table cellspacing="0" cellpadding="2" width="98" style='border: solid black 1px;'><tr><td style='font-size: 9px;'>
+$html
+</td></tr></table>
+END
+    ;
