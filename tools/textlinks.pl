7124aa5ba24aef02e589ae5783f7ba3ec02cd609
Björn Stenberg <bjorn@haxx.se>
2008-06-28 15:19:51 +0000

    Don't truncate the file on failed network transfer.
    git-svn-id: svn://svn.rockbox.org/rockbox/www@17832 a1c6a512-1295-4272-9138-f99709370657

--- tools/textlinks.pl
+++ tools/textlinks.pl
@@ -4,10 +4,13 @@ use open OUT => ":encoding('iso-8859-1')";
 
 # get text links from xml feed and produce html
 
+my $file = $ARGV[0];
+
 my $url = "http://update.livecustomer.net/?key=a539610bf93ee0795156fa7055d4b222&version=2.0.0&include_id=4696";
 
-my @output = `curl --silent --max-time 60 "$url"`;
+my @output = `curl --silent --max-time 60 "$url" -z $file`;
 my $output = join '', @output;
+exit if (length $output < 10);
 
 my @lines = split /<Link/i, $output;
 my @list;
@@ -26,7 +29,9 @@ for my $line (@lines) {
 
 my $html = join ",\n", @list;
 
-print <<END
+open(FILE, ">:encoding(UTF-8)", $file) or
+    die "Failed opening $file for writing: $!";
+print FILE <<END
 <p>&nbsp;<p>&nbsp;<p>&nbsp;<p>&nbsp;<p>&nbsp;<p>&nbsp;
 <p style='margin: 0 0 0 1px;'><img src='tlinkhead.gif' width=20 height=13>
 <table cellspacing="0" cellpadding="2" width="98" style='border: solid black 1px;'><tr><td style='font-size: 9px; font-family: arial, helvetica, sans-serif;'>
@@ -34,3 +39,5 @@ $html
 </td></tr></table>
 END
     ;
+
+close FILE;
