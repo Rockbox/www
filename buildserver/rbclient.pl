ea2b3d77d38334761e157211cea607c768be1eec
Björn Stenberg <bjorn@haxx.se>
2009-06-19 23:03:34 +0000

    Send HELLO with mostly proper params.
    git-svn-id: svn://svn.rockbox.org/rockbox/www@21358 a1c6a512-1295-4272-9138-f99709370657

--- buildserver/rbclient.pl
+++ buildserver/rbclient.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 
-use Socket;
-use IO::Handle;
+#use strict;
+use IO::Socket;
+use IO::Select;
 
 my $clientver = 1;
 my $username = "foobar";
@@ -14,18 +15,106 @@ socket(SOCKET, PF_INET, SOCK_STREAM, $proto) or die;
 
 my $port = 19999;
 
-my $sin = sockaddr_in($port, inet_aton("localhost"));
-connect(SOCKET, $sin) || die;
+my $sock = IO::Socket::INET->new(PeerAddr => 'localhost',
+                                 PeerPort => 19999,
+                                 Proto    => 'tcp',
+                                 Blocking => 0)
+    or die "$!";
 
-SOCKET->autoflush(1);
+my $auth = "$username:$password";
+my $speed = &bogomips;
+my $cpu = `uname -m`;
+chomp $cpu;
+my $os = `uname -o`;
+chomp $os;
 
-sub hello {
-    my $auth = "$username:$password";
-    print SOCKET "HELLO $clientver $auth $clientname $archlist\n";
+# Add the master socket to select mask
+my $read_set = new IO::Select();
+$read_set->add($sock);
+
+print $sock "HELLO $clientver $archlist $auth $clientname $cpu 32 $os $speed\n";
+
+# Mail loop active until ^C pressed
+my $done = 0;
+$SIG{INT} = sub { warn "received interrupt\n"; $done = 1; };
+
+while (not $done) {
+    my @handles = sort map $_->fileno, $read_set->handles;
+    my ($rh_set, $timeleft) =
+        IO::Select->select($read_set, undef, undef, 1);
+
+    foreach my $rh (@$rh_set) {
+        my $data;
+        my $len = $rh->read($data, 512);
+        
+        if ($len) {
+            $input .= $data;
+            
+            my $pos = index($input, "\n");
+            if($pos != -1) {
+                parsecmd($input);
+                $input = substr($input, $pos);
+            }
+        }
+    }
+
+    for my $id (sort {$a <=> $b} keys %builds) {
+        print "Starting build $builds{$id}{confargs}\n";
+    }
 }
 
-hello();
+#################################################
 
-while(<SOCKET>) {
-    print $_;
+sub bogomips
+{
+    open CPUINFO, "</proc/cpuinfo" or return 0;
+    my @lines = grep 'bogomips:', <CPUINFO>;
+    close CPUINFO;
+
+    my $bogomips = 0;
+    for (@lines) {
+        if (/bogomips\s*: (\d+)/) {
+            $bogomips += $1;
+        }
+    }
+
+    return $bogomips;
+}
+    
+sub _HELLO
+{
+}
+
+sub BUILD
+{
+    my ($id, $confargs, $rev, $zip, $mt) = split(' ', shift @_);
+
+    if (defined $builds{$id}) {
+        print SOCKET "_BUILD 0\n";
+        return;
+    }
+
+    $builds{$id}{confargs} = $confargs;
+    $builds{$id}{rev} = $rev;
+    $builds{$id}{zip} = $zip;
+    $builds{$id}{mt} = $mt;
+
+    print SOCKET "_BUILD $id\n";
+}
+
+sub parsecmd
+{
+    my ($cmdstr)=@_;
+    
+    if($cmdstr =~ /([_A-Z]*) (.*)/) {
+        my $func = $1;
+        my $rest = $2;
+        chomp $rest;
+        print "client: $func received\n";
+
+        &$func($rest);
+    }
+    else {
+        print "Client didn't recognize '$cmdstr'\n";
+    }
 }
