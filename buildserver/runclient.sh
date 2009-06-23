3d68c9f21a257856eee8c45f7bcaff205614d338
Daniel Stenberg <daniel@haxx.se>
2009-06-23 22:31:19 +0000

    eeek, clear off my dummy data
    git-svn-id: svn://svn.rockbox.org/rockbox/www@21482 a1c6a512-1295-4272-9138-f99709370657

--- buildserver/runclient.sh
+++ buildserver/runclient.sh
@@ -3,7 +3,7 @@ trap "exit" INT
 
 while true
 do
-    perl -s rbclient.pl -username=daniel -password=looser -archlist=arm,m68k,sh,mipsel,sdl -clientname=storebror
+    perl -s rbclient.pl -username= -password= -archlist=arm,m68k,sh,mipsel,sdl -clientname=
     res=$?
     if test "$res" -eq 22; then
       echo "Address the above issue(s), then restart!"
