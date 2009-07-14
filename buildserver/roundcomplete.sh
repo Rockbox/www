7fbc429eb29dd3dcdcf39e11b0aa838bafd6649c
Björn Stenberg <bjorn@haxx.se>
2009-07-14 21:37:08 +0000

    clientstats.pl now does its' own head+tail printout. Added mktitlepics.pl
    git-svn-id: svn://svn.rockbox.org/rockbox/www@21875 a1c6a512-1295-4272-9138-f99709370657

--- buildserver/roundcomplete.sh
+++ buildserver/roundcomplete.sh
@@ -4,9 +4,8 @@ rev=$1
 
 cat data/$rev*.size > data/$rev.sizes
 
-grep -v _PAGE_ head.html > data/$rev-clients.html
 perl clientstats.pl $rev >> data/$rev-clients.html
-cat foot.html >> data/$rev-clients.html
 
 perl showbuilds.pl > builds.html
 perl showsize.pl > sizes.html
+perl mktitlepics.pl
