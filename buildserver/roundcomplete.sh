e1589d60a39f046d180e265a0ba0f2ad3ac7e7d5
Björn Stenberg <bjorn@haxx.se>
2009-07-25 22:41:43 +0000

    Added data dir cleanup. Added build-info creation for rbutil.
    git-svn-id: svn://svn.rockbox.org/rockbox/www@22047 a1c6a512-1295-4272-9138-f99709370657

--- buildserver/roundcomplete.sh
+++ buildserver/roundcomplete.sh
@@ -2,10 +2,20 @@
 
 rev=$1
 
+# talk to rasher before removing this
 cat data/$rev*.size > data/$rev.sizes
 
-perl clientstats.pl $rev >> data/$rev-clients.html
+perl clientstats.pl $rev > data/$rev-clients.html
 
 perl showbuilds.pl > builds.html
 perl showsize.pl > sizes.html
 perl mktitlepics.pl
+perl cleanupdatadir.pl
+
+# make build-info for rbutil
+echo "[bleeding]" > build-info
+date +'timestamp = "%Y%m%dT%H%M%SZ"' >> build-info
+echo -n 'rev = "' >> build-info
+echo -n $rev >> build-info
+echo '"' >> build-info
+
