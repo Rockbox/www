dbde8d81d41c6de9656238b38fe3bacaa7791ec2
Björn Stenberg <bjorn@haxx.se>
2009-07-25 22:43:43 +0000

    Cut away functions that are now in rbmaster.pm. Moved from score to speed.
    git-svn-id: svn://svn.rockbox.org/rockbox/www@22048 a1c6a512-1295-4272-9138-f99709370657

--- buildserver/clientstats.pl
+++ buildserver/clientstats.pl
@@ -1,45 +1,19 @@
 #!/usr/bin/perl
 
 use DBI;
-
-eval 'require "secrets.pm"';
-
-sub nicehead {
-    my ($title)=@_;
-
-    open(READ, "<head.html");
-    while(<READ>) {
-        s/_PAGE_/$title/;
-        print $_;
-    }
-    close(READ);
-
-}
-
-sub nicefoot {
-    open(READ, "<foot.html");
-    while(<READ>) {
-        print $_;
-    }
-    close(READ);
-}
-
-my %builds;
+require 'rbmaster.pm';
 
 &getbuilds("builds");
 
 my $rev = $ARGV[0];
 
-my $dbpath = 'DBI:mysql:rockbox';
-my $db = DBI->connect($dbpath, $rb_dbuser, $rb_dbpwd);
-my $getclient_sth = $db->prepare("SELECT totscore, builds FROM clients WHERE name=?") or
-    warn "DBI: Can't prepare statement: ". $db->errstr;
+&db_connect();
 
-my $sth = $db->prepare("SELECT id,client,timeused FROM builds WHERE revision=?");
+my $sth = $db->prepare("SELECT id,client,timeused,ultime FROM builds WHERE revision=?");
 my $rows = $sth->execute($rev) + 0;
 if ($rows) {
-    while (my ($id, $client, $time) = $sth->fetchrow_array()) {
-        $clients{$client}{$id} = $time;
+    while (my ($id, $client, $time, $ultime) = $sth->fetchrow_array()) {
+        $clients{$client}{$id} = $time - $ultime;
         $score{$client} += $builds{$id}{score};
     }
 }
@@ -49,60 +23,28 @@ if ($rows) {
 printf("<p>For these $rows builds, the following %d build clients participated:\n",
        scalar keys %clients);
 
-print "<table><tr><th>Client</th> <th>Average<br>score</th> <th>Round<br>score</th> <th>Builds</th> <th>Total time</th> <th>All times</th> </tr>\n";
+print "<table><tr><th>Client</th> <th>Score</th> <th>Avg speed<br>(pts/sec)</th> <th>Round<br>speed</th> <th>Builds</th> <th>Total time</th> <th>All times</th> </tr>\n";
 
 for my $c (sort {$score{$b} <=> $score{$a}} keys %clients) {
-    my $avgscore = 0;
-    my $rows = $getclient_sth->execute($c);
-    if ($rows > 0) {
-        my ($score, $count) = $getclient_sth->fetchrow_array();
-        if ($count) {
-            $avgscore = int($score / $count);
-        }
-    }
-
-
+    my $speed = &getspeed($c);
     my $builds;
     my $times;
     my $total;
-    my $score;
     for my $id (keys %{$clients{$c}}) {
         my $t = $clients{$c}{$id};
         $total += $t;
         $times .= "$t ";
         $builds += 1;
     }
-    print "<tr> <td>$c</td> <td align=center>$avgscore</td> <td align=center>$score{$c}</td> <td align=center>$builds</td> <td align=center>$total</td> <td>$times</td> </tr>\n";
+    my $roundspeed = int($score{$c} / $total);
+    print "<tr> <td>$c</td> <td>$score{$c}</td> <td align=center>$speed</td> <td align=center>$roundspeed</td> <td align=center>$builds</td> <td align=center>$total</td> <td>$times</td> </tr>\n";
 }
 print "</table>\n";
 
-my $end = `tail -1000 logfile | grep "End of round $rev"`;
+my $end = `tail -100 logfile | grep "End of round $rev"`;
 if ($end =~ /seconds (\d+) wasted (\d+)/) {
     my $proc = int(100 * $2 / ($1 * scalar keys %clients));
     print "<p>This build round took $1 seconds. $2 cpu seconds ($proc%) were spent on cancelled builds.\n";
 }
 
 &nicefoot();
-
-sub getbuilds {
-    my ($filename)=@_;
-    open(F, "<$filename");
-    while(<F>) {
-        # sdl:nozip:recordersim:Recorder - Simulator:rockboxui:--target=recorder,--ram=2,--type=s
-        if($_ =~ /([^:]*):([^:]*):([^:]*):([^:]*):([^:]*):([^:]*):(\d+)/) {
-            my ($arch, $zip, $id, $name, $file, $confopts, $score) =
-                ($1, $2, $3, $4, $5, $6, $7);
-            $builds{$id}{'arch'}=$arch;
-            $builds{$id}{'zip'}=$zip;
-            $builds{$id}{'name'}=$name;
-            $builds{$id}{'file'}=$file;
-            $builds{$id}{'confopts'}=$confopts;
-            $builds{$id}{'score'}=$score;
-            $builds{$id}{'handcount'} = 0; # not handed out to anyone
-            $builds{$id}{'done'} = 0; # not done
-
-            push @buildids, $id;
-        }
-    }
-    close(F);
-}
