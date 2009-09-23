df76652c7c1ec471f17e6e780469f9f32cca7af9
Björn Stenberg <bjorn@haxx.se>
2009-09-23 10:55:56 +0000

    Moved svn commit log creation to update_front.sh
    git-svn-id: svn://svn.rockbox.org/rockbox/www@22796 a1c6a512-1295-4272-9138-f99709370657

--- update_front.sh
+++ update_front.sh
@@ -11,3 +11,13 @@ perl last20threads.pl 20 1 > threads.html
 # recent wiki edits
 perl recentwiki.pl 5 > recentwiki_front.html
 perl recentwiki.pl 20 > recentwiki.html
+
+# commits since last release
+grep -v _PAGE_  head.html > since33.html
+( cd ../trunk && svn log -v -r HEAD:21336 ) | perl tools/svnlog2html.pl >> since33.html
+cat foot.html >> since33.html
+
+# commits last four weeks
+grep -v _PAGE_  head.html > since-4weeks.html
+( cd ../trunk && svn log -v -r "HEAD:{`date --date='28 days ago' +'%Y-%m-%d'`}" ) | perl tools/svnlog2html.pl >> since-4weeks.html
+cat foot.html >> since-4weeks.html
