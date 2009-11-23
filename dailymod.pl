956e07e8f796d625f9b2db7d0f5420eb8d322308
Michael Chicoine <mc2739@gmail.com>
2009-11-23 19:30:47 +0000

    Add Sansa Clip v1 to current builds and manual web pages.
    git-svn-id: svn://svn.rockbox.org/rockbox/www@23724 a1c6a512-1295-4272-9138-f99709370657

--- dailymod.pl
+++ dailymod.pl
@@ -27,7 +27,7 @@ my @list=(
 
           "yh920", "yh925",
 
-          "sansac200", "sansae200", 
+          "sansac200", "clip", "sansae200", 
           "sansae200v2", "sansafuze",
 
           "gigabeatf",
