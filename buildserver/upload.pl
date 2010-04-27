1aac70dc6dee200e6b1fc5e5e1d720ef76230d91
Björn Stenberg <bjorn@haxx.se>
2010-04-27 10:59:55 +0000

    Fixed upload name mixup bug. Patch by Frank Gevaerts (FS#11221)
    git-svn-id: svn://svn.rockbox.org/rockbox/www@25738 a1c6a512-1295-4272-9138-f99709370657

--- buildserver/upload.pl
+++ buildserver/upload.pl
@@ -8,7 +8,7 @@ my $cwd = dirname $0;
 my $cgi = basename $0;
 
 my $filename = param("upfile");
-my $tmpfile = CGI::tmpFileName($filename);
+my $fh = CGI::upload("upfile");
 
 $filename =~ s/[\;\:\!\?\*\"\'\,\ ]/_/g;
 print STDERR "Uploading $filename\n";
@@ -27,12 +27,15 @@ if (-f "$destpath/$filename") {
     exit;
 }
 
-if (move($tmpfile, "$destpath/$filename")) {
-    chmod 0660, "$destpath/$filename"; # chmod ug+rw
+if (open OUTFILE, ">$destpath/$filename") {
+    while ($bytesread=read($fh,$buffer,1024)) {
+        print OUTFILE $buffer;
+    }
+    close OUTFILE;
     print "Status: 200 Upload successful\n";
 }
 else {
-    print "Status: 502 Move failed: $!\n";
+    print "Status: 502 File copy failed: $!\n";
 }
 
 print "\n$destpath/$filename\n";
