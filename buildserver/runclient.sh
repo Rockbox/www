70c0fa05abf2c232b36cd2c35a7b725131f81cd4
Alex Parker <rockbox@aeparker.com>
2012-05-05 21:45:24 +0100

    Buildserver: Set archlist to currently allowed values.

--- buildserver/runclient.sh
+++ buildserver/runclient.sh
@@ -6,7 +6,7 @@ do
     if [ -f "rbclient.pl.new" ]; then
         mv "rbclient.pl.new" "rbclient.pl"
     fi
-    perl -s rbclient.pl -username= -password= -clientname= -archlist=arm,m68k,sh,mipsel,sdl,arm-eabi-gcc444 -buildmaster=buildmaster.rockbox.org -port=19999
+    perl -s rbclient.pl -username= -password= -clientname= -archlist=m68k-gcc452,sh,mipsel,sdl,arm-eabi-gcc444,arm-ypr0-gcc446,android15 -buildmaster=buildmaster.rockbox.org -port=19999
     res=$?
     if test "$res" -eq 22; then
       echo "Address the above issue(s), then restart!"
