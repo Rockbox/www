abed54da5429f5d899bef40a6a78138ca59b2dc8
Björn Stenberg <bjorn@haxx.se>
2012-05-08 14:32:33 +0200

    Strip review tags from front page.

--- tools/gitlog2html.pl
+++ tools/gitlog2html.pl
@@ -10,7 +10,9 @@ my @mname = ('January', 'February', 'March', 'April', 'May',
              'November', 'December' );
 
 my %skip_tags = ('Change-Id' => 1,
-                 'git-svn-id' => 1);
+                 'git-svn-id' => 1,
+                 'Reviewed-on' => 1,
+                 'Reviewed-by' => 1);
 
 sub file2url {
     my ($file, $a, $i, $rev)=@_;
