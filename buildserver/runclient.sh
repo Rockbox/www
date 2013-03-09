51cd816cbe4f0f4f515b474a4be98b4d6f5606b0
Frank Gevaerts <frank@gevaerts.be>
2013-03-09 12:15:47 +0100

    Update runclient.sh to android16, and document archlist
    Change-Id: Id0446b3564824011f5e1aa7c9def50a1a1f78e85

--- buildserver/runclient.sh
+++ buildserver/runclient.sh
@@ -6,7 +6,18 @@ do
     if [ -f "rbclient.pl.new" ]; then
         mv "rbclient.pl.new" "rbclient.pl"
     fi
-    perl -s rbclient.pl -username= -password= -clientname= -archlist=m68k-gcc452,sh,mipsel,sdl,arm-eabi-gcc444,arm-ypr0-gcc446,android15 -buildmaster=buildmaster.rockbox.org -port=19999
+    # Possible values for archlist are:
+
+    # arm-eabi-gcc444 : needed for ARM-based traditional targets
+    # arm-ypr0-gcc446 : used for the Samsung YP-R0, and possibly later for similar hybrid application builds
+    # sh : SH-based players, i.e. the Archoses
+    # m68k-gcc452 : coldfire-based players
+    # mipsel : MIPS-based players
+    # sdl : Non-crosscompiled targets. Simulators, application, checkwps, database tool, ...
+    # android16 : Android port
+    # latex : manuual
+
+    perl -s rbclient.pl -username= -password= -clientname= -archlist=m68k-gcc452,sh,mipsel,sdl,arm-eabi-gcc444,arm-ypr0-gcc446,android16 -buildmaster=buildmaster.rockbox.org -port=19999
     res=$?
     if test "$res" -eq 22; then
       echo "Address the above issue(s), then restart!"
