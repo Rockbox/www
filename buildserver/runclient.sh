b3518a0dcd8efccbd543ce7e1424f5babdf6004d
Solomon Peachy <pizza@shaftnet.org>
2018-10-09 22:23:07 -0400

    fix typos in vx747 bootloader build definition

--- buildserver/runclient.sh
+++ buildserver/runclient.sh
@@ -12,13 +12,13 @@ do
     # arm-rb-gcc494   : linux based sony players, Samsung YP-R0 YP-R1
     # sh : SH-based players, i.e. the Archoses
     # m68k-gcc452 : coldfire-based players
-    # mipsel-494 : MIPS-based players
+    # mipsel-gcc494 : MIPS-based players
     # mipsel-rb-gcc494: linux based MIPS players i.e Agptek Rocker (Benjie T6)
     # sdl : Non-crosscompiled targets. Simulators, application, checkwps, database tool, ...
     # android16 : Android port
     # latex : manuual
 
-    perl -s rbclient.pl -username= -password= -clientname= -archlist=m68k-gcc452,sh,mipsel,sdl,arm-eabi-gcc444,arm-ypr0-gcc446,android16 -buildmaster=buildmaster.rockbox.org -port=19999
+    perl -s rbclient.pl -username= -password= -clientname= -archlist=m68k-gcc452,sh,mipsel-gcc494,sdl,arm-eabi-gcc444,arm-ypr0-gcc446,android16 -buildmaster=buildmaster.rockbox.org -port=19999
     res=$?
     if test "$res" -eq 22; then
       echo "Address the above issue(s), then restart!"
