04e41ad93dea2adf9d7294167ed0eaa51cf9a034
Solomon Peachy <pizza@shaftnet.org>
2020-06-25 18:53:33 -0400

    buildserver:  unstage translate/themesite updates at the start of a new round
    (in other words, don't do a themesite/translate update until we're done
     with all pending buildserver rounds)

--- buildserver/hooks/roundstart.sh
+++ buildserver/hooks/roundstart.sh
@@ -3,7 +3,12 @@
 rev=$1
 
 touch data/build_running
+
 perl showbuilds.pl $rev > builds.html.new && mv builds.html.new builds.html
+
+# If we have updates staged, unstage them until all active rounds are done
+rm -f ../../translate/need_update
+rm -f ../../themes/need_update
 
 if [ -e rcbuild.hash ]; then
     hash=`cat rcbuild.hash`
