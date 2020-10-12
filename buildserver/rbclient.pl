9606657dd39ced216a6d8226e200321a02ca05d3
Solomon Peachy <pizza@shaftnet.org>
2020-10-11 20:56:15 -0400

    Get rid of the sh tooling.
    Change-Id: I9198291796fc33256631385b89c13f3e47bc9a6b

--- buildserver/rbclient.pl
+++ buildserver/rbclient.pl
@@ -541,14 +541,16 @@ sub testsystem
         "android-ndk10sdk19" => { "cat $ENV{ANDROID_NDK_PATH}/RELEASE.TXT" => "r10",
 				      "$ENV{ANDROID_SDK_PATH}/tools/android list target" => "API level: 19" },
 
+	# Nuked obsolete targets
+        "sh" => { "sh-elf-gcc --version", "4.0.3" },
+        "sh-gcc494" => { "sh-elf-gcc --version", "4.9.4" },
+
 	# Native targets (active tooling)
         "arm-eabi-gcc444" => { "arm-elf-eabi-gcc --version", "4.4.4" },
-        "sh" => { "sh-elf-gcc --version", "4.0.3" },
         "m68k-gcc452" => { "m68k-elf-gcc --version", "4.5.2" },
         "mipsel-gcc494" => { "mipsel-elf-gcc --version", "4.9.4" },
 
         # Native targets (Upcoming tooling)
-        "sh-gcc494" => { "sh-elf-gcc --version", "4.9.4" },
         "arm-eabl-gcc494" => { "arm-elf-eabi-gcc --version", "4.9.4" },
         "m68k-gcc494" => { "m68k-elf-gcc --version", "4.9.4" },
 
--- buildserver/runclient.sh
+++ buildserver/runclient.sh
@@ -11,12 +11,10 @@ do
 
     ####### Native (Actively used)
     # arm-eabi-gcc444        : needed for ARM-based traditional targets
-    # sh                     : SH-based players, i.e. the Archoses
     # m68k-gcc452            : coldfire-based players
     # mipsel-gcc494          : MIPS-based players
 
     ####### Native (Newer toolchains that will be deployed soon)
-    # sh-gcc494              : SH-based players, i.e. the Archoses
     # m68k-gcc494            : m68k-based players
     # arm-eabi-gcc494        | arm-based players
 
@@ -33,7 +31,7 @@ do
     # android-ndk10          : Android NDK 10e, eg iBasso dx50/dx90
     # android-ndk10sdk19     : Android NDK 10e and SDK+tools supporting API 19 (4.4/KitKat)
 
-    perl -s rbclient.pl -username= -password= -clientname= -archlist=m68k-gcc452,sh,mipsel-gcc494,sdl,arm-eabi-gcc444,arm-ypr0-gcc446,android16 -buildmaster=buildmaster.rockbox.org -port=19999
+    perl -s rbclient.pl -username= -password= -clientname= -archlist=m68k-gcc452,mipsel-gcc494,sdl,arm-eabi-gcc444,arm-ypr0-gcc446,android16 -buildmaster=buildmaster.rockbox.org -port=19999
     res=$?
     if test "$res" -eq 22; then
       echo "Address the above issue(s), then restart!"
