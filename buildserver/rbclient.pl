d48c224dc0dc724845a31b203277353c414e7c83
Solomon Peachy <pizza@shaftnet.org>
2020-10-29 16:20:17 -0400

    rbclient.pl:  change to the correct directory prior to cleaning up.
    Change-Id: Ib495e96e98f05cb53b487eaa15bf512acddf1381

--- buildserver/rbclient.pl
+++ buildserver/rbclient.pl
@@ -19,7 +19,7 @@ my $perlfile = "rbclient.pl";
 # Increment this to have the buildmaster auto-update the cluster.
 # Remember to get someone to increment the corresponding value in
 # rbmaster.conf on the server!
-my $revision = 67;
+my $revision = 68;
 my $cwd = `pwd`;
 chomp $cwd;
 
@@ -371,6 +371,8 @@ sub startbuild
         print $pipe "done $id $$ $ultime $ulsize $ok\n";
         close $pipe;
 
+	# Clean up
+	chdir "..";
         if (-d "build-$id") {
             rmtree "build-$id";
         }
