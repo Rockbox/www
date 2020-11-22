340ac0447a9415f7e076df14a79293977b752315
Solomon Peachy <pizza@shaftnet.org>
2020-11-22 12:03:03 -0500

    build-info:  Rework format of development builds.

--- buildserver/devbuilds.pl
+++ buildserver/devbuilds.pl
@@ -1,7 +1,41 @@
 #!/usr/bin/perl
 
 require "../rockbox.pm";
-require "./devbuilds.pm";
+
+my $basedir = "/home/rockbox/build";
+my %revs;
+
+sub buildtable {
+    print "<p><table class='rockbox' cellpadding=\"0\"><tr valign=top>\n";
+    for my $m (usablebuilds()) {
+        if( ($m =~ /source/ ) || -r "$basedir/data/rockbox-$m.zip") {
+            my $pack="data/rockbox-$m.zip";
+            my $name= $builds{$m}{name},
+            my $version = "";
+            my $rev="broken?";
+	    my @ver = `unzip -p data/rockbox-$m.zip .rockbox/rockbox-info.txt`;
+	    $version = (grep /^Version/, @ver)[0];
+	    chomp $version;
+	    if($version =~ /^Version: *(\w+)/) {
+		$rev = $1;
+	    }
+	    else {
+		print "<br>Failed regex: $version\n";
+	    }
+	    $revs{$m} = $rev;
+            if($col++ > 7) {
+                print "</tr><tr valign=\"top\">";
+                $col=1;
+            }
+            printf("<td align='center'><a href=\"$pack\" title=\"$name\"><img border=\"0\" src=\"//www.rockbox.org%s\" alt=\"$name\"><p>$name</a><br><small>$rev</small></td>\n",
+                   playerpic($m));
+        }
+        else {
+            #print "<br>MISSING: $basedir/data/rockbox-$m.zip\n";
+        }
+    }
+    print "</tr></table>";
+}
 
 header_b("Rockbox Development Builds");
 
@@ -13,7 +47,7 @@ print <<HEAD
  builds are not currently showing, wait 5-10 minutes and then refresh the page.
 <p>
  Since these builds are generated from actively developed source, at times
- they may be buggy or even unusable. 
+ they may be buggy or even unusable.
  We appreciate your feedback on any issues you may encounter.
 
 $beware
@@ -22,7 +56,7 @@ $beware
  For a stable build, <a href="//www.rockbox.org/download/">download the latest stable release</a>.
 
 <p>
-<a href="dev.cgi">Autobuilder details</a> &middot; 
+<a href="dev.cgi">Autobuilder details</a> &middot;
 <a href="//www.rockbox.org/manual.shtml">Manuals</a> &middot;
 <a href="//www.rockbox.org/daily.shtml">Archived developer builds</a>
 HEAD
@@ -31,3 +65,16 @@ HEAD
 buildtable();
 
 footer();
+
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = gmtime(time);
+my $date=sprintf("%04d%02d%02dT%02d%02d%02dZ", $year + 1900, $mon + 1, $mday, $hour, $min, $sec);
+
+open(F, ">build-info.new");
+print F "[bleeding]\n";
+print F "timestamp=\"$date\"\n";
+print F "rev=\"$ARGV[0]\"\n";
+print F "[development]\n";
+for my $m (usablebuilds()) {
+    print F "$m=$revs{$m},https://build.rockbox.org/data/rockbox-$m.zip\n";
+}
+close(F);
--- buildserver/hooks/roundcomplete.sh
+++ buildserver/hooks/roundcomplete.sh
@@ -21,15 +21,8 @@ else
     perl showbuilds.pl > builds.html.new && mv builds.html.new builds.html
     perl showbuilds.pl 1 > builds_all.html.new && mv builds_all.html.new builds_all.html
     perl showsize.pl > sizes.html.new && mv sizes.html.new sizes.html
-    perl devbuilds.pl > devbuilds.html.new && mv devbuilds.html.new devbuilds.html
-#    perl mktitlepics.pl
-
-    # make build-info for rbutil
-    echo "[bleeding]" > build-info.new
-    date +'timestamp = "%Y%m%dT%H%M%SZ"' >> build-info.new
-    echo -n 'rev = "' >> build-info.new
-    echo -n $rev >> build-info.new
-    echo '"' >> build-info.new
+    perl devbuilds.pl $rev > devbuilds.html.new && mv devbuilds.html.new devbuilds.html
+    # created by devbuilds.pl
     mv build-info.new /home/rockbox/download/build-info.devbuild
 
     # udpate local git repo
