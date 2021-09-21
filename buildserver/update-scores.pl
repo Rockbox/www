29396344c79c13405b1b86dd6ba0abbca6333614
Solomon Peachy <pizza@shaftnet.org>
2021-09-21 06:49:11 -0400

    buildserver:  Add a script to automatically recompute all scores
    Change-Id: Ifffa999ea0468d412a874ac98c762f99336f54c7

---
+++ buildserver/update-scores.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+#
+# This script walks through the 'builds' file, building each one in turn
+# and updating the 'scores' value.
+#
+# As the scores are only relevant relative to each other, it is important that
+# all scores are generated using the same build system, using a warm filesystem cache,
+# and are periodically regenerated, especially in the face of new/updated toolchains.
+#
+
+my $jobs = `nproc` + 0;
+
+while (<STDIN>) {
+    if (/^\#/) {
+        print;
+        next;
+    }
+    chomp;
+    my @row = split(/:/);
+
+    my $make = $row[2];
+    my $oldscore = $row[5];
+    my $build = $row[6];
+
+    my @tmp = split(/&/,$build);
+    my $conf = $tmp[0] . " --no-ccache ";
+    my $build = $tmp[2] . " -j$jobs ";
+    $build =~ s/(.*)zip/$1/;
+
+    system("$conf > /dev/null");
+    system("make clean > /dev/null");
+    my $score = `(/usr/bin/time -f"%U+%S" $build >/dev/null) 2>&1 | bc -l` * 100;
+    system("make clean > /dev/null");
+
+    $row[5] = $score;
+
+    my $newrow = join(':',@row);
+    print "$newrow\n";
+}
