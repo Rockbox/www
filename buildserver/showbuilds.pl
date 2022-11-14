f1bebf9c28be9473b21758d2ee019ba8a12e4730
Solomon Peachy <pizza@shaftnet.org>
2022-11-14 09:06:05 -0500

    builds:  Fix typo in URL.

--- buildserver/showbuilds.pl
+++ buildserver/showbuilds.pl
@@ -151,7 +151,7 @@ if($buildrev) {
     }
 
     my $shortrev = substr($buildrev, 0, 8);
-    printf("<tr><td align=\"center\"><a class=\"bstamp\" href=\"//git.rockbox.org/cgit/rockbox.git/commit?id=$buildrev\">$shortrev</a><td colspan=\"3\" align=\"center\">(in progress)</td><td class=\"building\" colspan=\"%d\">$text</td></tr>\n",
+    printf("<tr><td align=\"center\"><a class=\"bstamp\" href=\"//git.rockbox.org/cgit/rockbox.git/commit/?id=$buildrev\">$shortrev</a><td colspan=\"3\" align=\"center\">(in progress)</td><td class=\"building\" colspan=\"%d\">$text</td></tr>\n",
            $numbuilds);
 }
 #################
