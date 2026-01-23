Rockbox/www

commit 5766a32edac3323bf4b3256ef2fa21b588e6c197

buildmaster:  Simplify up the speculative build stuff
Should be no functional change.

Change-Id: I79f0cf09540e48f51f2b4de365ad9a8254c864d5

buildserver/rbmaster.pl
@@ -1364,11 +1364,10 @@ sub start_next_build($)
         }
     }
 
-    # queue is empty. how can I help?
+    ### queue is empty. how can I help?
 
     # any abandoned builds I can do?
-    if ($abandoned_builds)
-    {
+    if ($abandoned_builds) {
         for my $id (&bigbuilds) {
             if (client_can_build($cl, $id) and !$builds{$id}{assigned}) {
                 $client{$cl}{queue}{$id} = 1;
@@ -1384,29 +1383,28 @@ sub start_next_build($)
     if (1) {
         # help with other builds, speculatively
         for my $id (&smallbuilds) {
+            next if (!$builds{$id}{done});
             next if (!client_can_build($cl, $id));
             next if (defined $client{$cl}{btime}{$id});
             # don't start any build that would take >66% of round time
             if ($estimated_time) {
                 next if ($client{$cl}{roundspeed} and ($builds{$id}{score} / $client{$cl}{roundspeed} > $estimated_time * 2 / 3));
             }
-            if (!$builds{$id}{done}) {
-                if ($builds{$id}{handcount} == 0) {
-                    dlog "$cli does unstarted $id";
-                } else {
-                    if (!$speculative) {
-                        message "Speculative building started";
-                        $speculative = 1;
-                    }
 
-                    # don't start building this if someone faster
-                    # is already building it
-                    # XXX this is broken, I think.
-                    #next if ($builds{$id}{topspeed} > $client{$cl}{speed})
+	    # don't start building this if someone faster
+	    # is already building it
+	    #next if ($builds{$id}{topspeed} > $client{$cl}{speed})
+
+            if ($builds{$id}{handcount} == 0) {
+                dlog "$cli does unstarted $id";
+            } else {
+                if (!$speculative) {
+                    message "Speculative building started";
+                    $speculative = 1;
                 }
-                &build($cl, $id);
-                return;
             }
+            &build($cl, $id);
+            return;
         }
     }
 

commit 48d7e10ed851ce92fe882e439d31643bebeb93d6

buildmaster:  Fix an inverted test in the speculative build code
regression from 5766a32edac3323bf4b3256ef2fa21b588e6c197

Change-Id: I94fd4586104ea52be3da166a7ffc7b5e506aea95

buildserver/rbmaster.pl
@@ -1385,7 +1385,7 @@ sub start_next_build($)
     if (1) {
         # help with other builds, speculatively
         for my $id (&smallbuilds) {
-            next if (!$builds{$id}{done});
+            next if ($builds{$id}{done});
             next if (!client_can_build($cl, $id));
             next if (defined $client{$cl}{btime}{$id});
             # don't start any build that would take >66% of round time